98d3430b4f6d5695b602c01184fa2616076d2abc
Jonatan Fragoso <jonatanfragoso@gmail.com>
2023-05-03 22:41:01 -0500

    aula 7 concluida

---
+++ aula_7.py
@@ -0,0 +1,13 @@
+# Aula sobre variáveis
+# PEP8: inicie variáveis com letras minúsculas,
+# pode usar números e _.
+
+nome_completo = 'Jonatan Fragoso'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Jonatan'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome: ', nome, 'Idade: ', idade)
+print('É maior de idade? ', maior_de_idade)
