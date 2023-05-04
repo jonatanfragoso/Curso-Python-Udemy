624f66a31e11d7fece2d9d579f159be9d48c3fe9
Jonatan Fragoso <jonatanfragoso@gmail.com>
2023-05-03 22:32:16 -0500

    aula 6 concluida

---
+++ aula_6.py
@@ -0,0 +1,10 @@
+#conversão de tipos, coerção.
+#type convertion, typecasting, coercion
+
+print(1+1)
+print('a'+'b')
+
+print(int('1'), type(int('1')))
+print(str(11)+'a')
+print(bool(' '))
+print(bool(''))
